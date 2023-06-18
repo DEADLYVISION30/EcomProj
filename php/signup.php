2784042b3b99c807bea21cba8b7c30fb8263a2cb
Ziyan Ali Murtaza <ziyanali6@gmail.com>
2023-06-18 14:54:29 +0500

    database generated

--- php/signup.php
+++ php/signup.php
@@ -1,2 +1,12 @@
 <?php
-echo "Hello World";
+$name = $_POST["name"];
+$email = $_POST["email"];
+$username = $_POST["Username"];
+$password = $_POST["Pass"];
+$date = $_POST["date"];
+$link = mysqli_connect("localhost", "root", "", "ecommerce");
+if ($link->connect_error) {
+    die("Connection Unsuccessful");
+} else {
+    echo "Connected";
+}
