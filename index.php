9231b5df7bf9cc9c476fe6936c9712d470208b91
Ziyan Ali Murtaza <ziyanali6@gmail.com>
2023-06-24 23:21:50 +0500

    home page

--- index.php
+++ index.php
@@ -9,14 +9,6 @@ session_start();
 
 <body>
 
-	<?php
-	if ($_SESSION["email"]) {
-	?>
-		Welcome <?php echo $_SESSION["email"]; ?>. Click here to <a href="logout.php"><button>Logout</button>
-		<?php
-	} else
-		echo "<h1>Please login first .</h1>";
-		?>
 </body>
 <?php
 session_destroy();
