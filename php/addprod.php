68909091426fc1d4c1df68ad119518713d9e2300
Ziyan Ali Murtaza <ziyanali6@gmail.com>
2023-06-24 17:56:33 +0500

    product added

--- php/addprod.php
+++ php/addprod.php
@@ -6,41 +6,43 @@ if ($link->connect_error) {
     $productN = $_POST["Pname"];
     $price = $_POST['Pprice'];
     $category = $_POST['category'];
+    $description = $_POST['ProductDescription'];
     $fileName = basename($_FILES["files"]["name"]);
     $submit = $_POST["submit"];
     $targetdir = "../products/";
     $targetfilepath = $targetdir . $fileName;
     $filetype = pathinfo($targetfilepath, PATHINFO_EXTENSION);
     if (isset($_POST["submit"]) && !empty($_FILES["files"]["name"])) {
-        $query = "INSERT INTO products (ProductName,ProductDescription,Category,Image) values('$productN','$price','$category','$fileName')";
-        if ($link->query($query) && move_uploaded_file($_FILES["files"]["tmp_name"], $targetfilepath)) {
-            echo "Product Added";
-        } else {
-            echo ("There was an error uploading the file.
-            File upload returned error code: " . $_FILES["files"]["error"]);
-        }
-    }
-}
-?>
-<html>
-
-<head>
-    <style>
-        <?php include '../css/addprod.css'
-        ?>
-    </style>
-</head>
-
-<body>
-    <div>
+        $query = "INSERT INTO products (ProductName,Price,ProductDescription,Category,Image) values('$productN','$price','$description','$category','$fileName')"; ?>
         <?php
-        $q = "SELECT * FROM products WHERE ID=9";
-        $res = $link->query($q);
-        $result = mysqli_fetch_array($res);
-        ?>
-        <img style=" width:444px;" src="../products/<?php echo $result['Image']; ?>">
-        <h1> ProductName : <?php echo $result["ProductName"] ?></h1>
-    </div>
-</body>
+        if ($link->query($query) && move_uploaded_file($_FILES["files"]["tmp_name"], $targetfilepath)) {
+        ?><html>
 
-</html>
+            <head>
+                <style>
+                    <?php include '../css/addprod.css'
+                    ?>
+                </style>
+            </head>
+
+            <body>
+                <div>
+                    <?php
+                    $q = "SELECT * FROM products";
+                    $res = $link->query($q);
+                    $result = mysqli_fetch_array($res);
+                    ?>
+                    <img style=" width:444px;" src="../products/<?php echo $result['Image']; ?>">
+                    <h1> ProductName : <?php echo $result["ProductName"] ?></h1>
+                    <h1> ProductDescription : <?php echo $result["ProductDescription"] ?></h1>
+                    <h1> ProductCategory : <?php echo $result["Category"] ?></h1>
+                </div>
+            </body>
+
+            </html><?php
+                } else {
+                    echo ("There was an error uploading the file.
+File upload returned error code: " . $_FILES["files"]["error"]);
+                }
+            }
+        } ?>
