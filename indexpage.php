d41c47ec98f743cec5e186a170f5ad19c6a2c5db
Ziyan Ali Murtaza <ziyanali6@gmail.com>
2023-07-02 17:06:40 +0500

    page changes

--- indexpage.php
+++ indexpage.php
@@ -1,23 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>title</title>
-    <link rel="stylesheet" href="./css/homepage.css" />
-  </head>
-  <body>
-    <div id="nav-bar">
-      <header>
-        <ul>
-          <li><a>Home</a></li>
-          <li><a>Contact</a></li>
-          <li><a>About us</a></li>
-          <li><a class="active">Login</a></li>
-          <li><a href="./signup.html">Signup</a></li>
-        </ul>
-      </header>
-    </div>
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>title</title>
+  <link rel="stylesheet" href="./css/homepage.css" />
+</head>
+
+<body>
+  <div id="nav-bar">
+    <header>
+      <ul>
+        <li><a>Home</a></li>
+        <li><a>Contact</a></li>
+        <li><a>About us</a></li>
+        <li><a class="active">Login</a></li>
+        <li><a href="./signup.html">Signup</a></li>
+      </ul>
+    </header>
+  </div>
+  <form>
     <div id="hero-image">
       <img alt="Laptop Image" src="./images/pexels-pixabay-356056.jpg" />
     </div>
@@ -31,6 +33,7 @@
           <span>Laptop</span>
         </div>
       </a>
+      </button>
       <div class="cats">
         <div class="img">
           <img alt="Mobile Phone img" src="./images/R.jpg" />
@@ -44,5 +47,7 @@
         <span>House Hold</span>
       </div>
     </div>
-  </body>
+  </form>
+</body>
+
 </html>
